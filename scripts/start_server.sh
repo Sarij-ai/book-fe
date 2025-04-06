59669da19f27a9bef274665f0743a11fd41b2632
Burhan <aicruit@Dev1s-MacBook-Pro.local>
2025-04-06 20:25:26 +0500

    Update config-fix-2

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /var/www/app/frontend
-pm2 start "npm start" --name paismo-fe -o /var/log/app/frontend/out.log -e /var/log/app/frontend/error.log
+pm2 start "npm start" --name book-fe -o /var/log/app/frontend/out.log -e /var/log/app/frontend/error.log
